d5befec668b69fe85625badb3671654aaf90e338
zacmiki <95082573+zacmiki@users.noreply.github.com>
2024-05-16 15:14:44 +0200

    Update hcpfunctions.py

--- hcpfunctions.py
+++ hcpfunctions.py
@@ -10,21 +10,20 @@ def loadcoursetable(dff):
     relevant_columns = ['Date_String', 'Gara', 'Stbl', 'AGS', 'SD', 'Index Nuovo']
     strippeddf = filtered_df[relevant_columns].copy()
     
-    # Extract the values of 'Index Nuovo' from the filtered DataFrame and convert to a NumPy array
-    #valid_results = filtered_df['Index Nuovo'].values
-    #sorted_valid_results = np.sort(valid_results)
-    # Select the 8 lowest values
-    #best_8 = sorted_valid_results[:8]
+    # Reset the index of strippeddf
+    strippeddf = strippeddf.reset_index(drop=True)
     
     #doing the same in one line
-    best_8 = strippeddf.nlargest(8, 'Stbl')
+    #best_8 = strippeddf.nlargest(8, 'Stbl')
+    best_8 = strippeddf.nlargest(8, 'SD')
+    worst_8 = strippeddf.nsmallest(8, 'SD')
     
-    min_stbl_index = best_8['Stbl'].idxmin()
-    worstofbest = best_8.loc[min_stbl_index]
+    min_SD_index = best_8['SD'].idxmin()
+    worstofbest = best_8.loc[min_SD_index]
     
     element_number = strippeddf.index[strippeddf.eq(worstofbest).all(axis=1)][0]
-    
-    nextgoodone = strippeddf.iloc[element_number]
+    nextgoodone = strippeddf.iloc[(min_SD_index-1)]
+
     
     strippeddf = strippeddf.rename(columns={'Index Nuovo': 'New EGA'})
     strippeddf = strippeddf.rename(columns={'Date_String': 'Date'})
@@ -40,11 +39,11 @@ def loadcoursetable(dff):
     st.success(f"Your Current HCP is: {current_handicap} - Best handicap: {best_handicap}", icon="⛳️",)
     st.info(f"Your Next EXPIRING Round is" )
     st.subheader(f" {strippeddf.iloc[-1]['Gara']}")
-    st.subheader(f"Date: {strippeddf.iloc[-1]['Date']} - Stablefod Result = {strippeddf.iloc[-1]['Stbl']}")
+    st.subheader(f"Date: {strippeddf.iloc[-1]['Date']} - Stableford = {strippeddf.iloc[-1]['Stbl']} - SD = {strippeddf.iloc[-1]['Stbl']}")
     st.divider()
     
-    st.info(f"""Apparently you have {20 - element_number} games to play before you lose your next valid round which is  \n
-    	{nextgoodone['Gara']} - Stbl = {nextgoodone['Stbl']}""")
+    st.info(f"""Apparently you have {20 - min_SD_index} games to play before you lose your next valid round which is  \n
+    	{worst_8.iloc[-1]['Gara']} - Stbl = {worst_8.iloc[-1]['Stbl']} - SD = {worst_8.iloc[-1]['SD']}""")
  
     st.subheader("Last 20 VALID Rounds")
     st.write("You can sort the table by clicking on the column name to  \n ... in case you want to what troubles are you running into, in your golfing future ...")
@@ -54,7 +53,7 @@ def loadcoursetable(dff):
     
     st.divider()
     st.subheader("Last 8 Valid Rounds (scorewise))")
-    st.write(best_8)
+    st.write(worst_8)
     
     '''
     st.write(worstofbest)
