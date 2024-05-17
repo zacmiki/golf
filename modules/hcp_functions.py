312392e7be10d3900b75133d6b04e8b23d4efe7e
Jac-Zac <jacopozac@icloud.com>
2024-05-17 11:01:36 +0200

    Done needs only to compute handicap

--- modules/hcp_functions.py
+++ modules/hcp_functions.py
@@ -1,61 +1,75 @@
 import streamlit as st
+
+
 def loadcoursetable(dff):
-    import pandas as pd
     import numpy as np
+    import pandas as pd
 
     df = pd.DataFrame(dff)
-    
+
     # Filter the DataFrame to select the first 20 elements where 'SD' is not NaN
-    filtered_df = df.dropna(subset=['SD']).head(20)
-    relevant_columns = ['Date_String', 'Gara', 'Stbl', 'AGS', 'SD', 'Index Nuovo']
+    filtered_df = df.dropna(subset=["SD"]).head(20)
+    relevant_columns = ["Date_String", "Gara", "Stbl", "AGS", "SD", "Index Nuovo"]
     strippeddf = filtered_df[relevant_columns].copy()
-    
+
     # Reset the index of strippeddf
     strippeddf = strippeddf.reset_index(drop=True)
-    
-    #doing the same in one line
-    #best_8 = strippeddf.nlargest(8, 'Stbl')
-    best_8 = strippeddf.nlargest(8, 'SD')
-    worst_8 = strippeddf.nsmallest(8, 'SD')
-    
-    min_SD_index = best_8['SD'].idxmin()
-    worstofbest = best_8.loc[min_SD_index]
-    
-    element_number = strippeddf.index[strippeddf.eq(worstofbest).all(axis=1)][0]
-    nextgoodone = strippeddf.iloc[(min_SD_index-1)]
 
-    
-    strippeddf = strippeddf.rename(columns={'Index Nuovo': 'New EGA'})
-    strippeddf = strippeddf.rename(columns={'Date_String': 'Date'})
+    # doing the same in one line
+    # best_8 = strippeddf.nlargest(8, 'Stbl')
+    best_8 = strippeddf.nlargest(8, "SD")
+    worst_8 = strippeddf.nsmallest(8, "SD")
+
+    min_SD_index = best_8["SD"].idxmin()
+    worstofbest = best_8.loc[min_SD_index]
+
+    element_number = strippeddf.index[strippeddf.eq(worstofbest).all(axis=1)][0]
+    nextgoodone = strippeddf.iloc[(min_SD_index - 1)]
+
+    strippeddf = strippeddf.rename(columns={"Index Nuovo": "New EGA"})
+    strippeddf = strippeddf.rename(columns={"Date_String": "Date"})
     # --------- PAGE LAYOUT
-    
+
     st.title("Handicap Manager")
     st.divider()
 
     current_handicap = df["Index Nuovo"][0]
     best_handicap = df["Index Nuovo"].min()
     st.subheader(f"Tesserato {df['Tesserato'][0]}")
-    
-    st.success(f"Your Current HCP is: {current_handicap} - Best handicap: {best_handicap}", icon="⛳️",)
-    st.info(f"Your Next EXPIRING Round is" )
+
+    st.success(
+        f"Your Current HCP is: {current_handicap} - Best handicap: {best_handicap}",
+        icon="⛳️",
+    )
+    st.info(f"Your Next EXPIRING Round is")
     st.subheader(f" {strippeddf.iloc[-1]['Gara']}")
-    st.subheader(f"Date: {strippeddf.iloc[-1]['Date']} - Stableford = {strippeddf.iloc[-1]['Stbl']} - SD = {strippeddf.iloc[-1]['SD']}")
+    st.subheader(
+        f"Date: {strippeddf.iloc[-1]['Date']} - Stableford = {strippeddf.iloc[-1]['Stbl']} - SD = {strippeddf.iloc[-1]['SD']}"
+    )
     st.divider()
-    
-    st.info(f"""Apparently you have {20 - min_SD_index} games to play before you lose your next valid round which is  \n
-    	{worst_8.iloc[-1]['Gara']} - Stbl = {worst_8.iloc[-1]['Stbl']} - SD = {worst_8.iloc[-1]['SD']}""")
- 
+
+    st.info(
+        f"""Apparently you have {20 - min_SD_index} games to play before you lose your next valid round which is  \n
+    	{worst_8.iloc[-1]['Gara']} - Stbl = {worst_8.iloc[-1]['Stbl']} - SD = {worst_8.iloc[-1]['SD']}"""
+    )
+
     st.subheader("Last 20 VALID Rounds")
-    st.write("You can sort the table by clicking on the column name to  \n ... in case you want to what troubles are you running into, in your golfing future ...")
+    st.write(
+        "You can sort the table by clicking on the column name to  \n ... in case you want to what troubles are you running into, in your golfing future ..."
+    )
     st.write(strippeddf)
-    
-    #st.write(st.write(st.session_state.df))
-    
+
+    # st.write(st.write(st.session_state.df))
+
     st.divider()
     st.subheader("Last 8 Valid Rounds (scorewise))")
     st.write(worst_8)
-    
-    '''
+
+    """
     st.write(worstofbest)
     st.write(element_number)
-    '''
+    """
+
+
+def compute_handicap(course_handicap):
+    pass
--- modules/login_federgolf.py
+++ modules/login_federgolf.py
@@ -3,6 +3,8 @@ import requests
 import streamlit as st
 from bs4 import BeautifulSoup
 
+from .hcp_functions import compute_handicap
+
 
 # Login and call the function to extract data
 def login(username, password):
@@ -321,92 +323,97 @@ def handicap_request():
 
     response = requests.post(url, headers=headers, data=data)
 
-    # Get the soup and select the Percorso
-    soup = BeautifulSoup(response.text, "html.parser")
-    course_options = {}
-    course_select = soup.select("#ddlPercorso option")
-    for option in course_select:
-        name = option.text.strip()
-        value = option["value"]
-        course_options[name] = value
-
-    # Dropdown for selecting the course
-    selected_course = st.selectbox("Select Course", options=list(course_options.keys()))
-
-    # Getting the value based on the selected course name
-    selected_course_value = course_options[selected_course]
-
-    # Dropdown for Giallo and Rosso
-    tee_color = st.selectbox(
-        "Select Tee Color",
-        options=["Bianco", "Giallo", "Verde", "Blu", "Rosso", "Arancio"],
-    )
-
-    # Text field for handicap
-    handicap = st.text_input("Enter Handicap")
-
-    # Button to trigger the request
-    if st.button("Compute Handicap"):
-        url = "https://areariservata.federgolf.it/CourseHandicapCalc/Calc"
-
-        headers = {
-            "Host": "areariservata.federgolf.it",
-            "Cookie": f"ASP.NET_SessionId={st.session_state.session_id}; __RequestVerificationToken={st.session_state.request_verification_token}; ARRAffinity={st.session_state.arraffinity}; ARRAffinitySameSite={st.session_state.arraffinity_same_site}",
-            "Content-Length": "260",
-            "Cache-Control": "max-age=0",
-            "Sec-Ch-Ua": '"Not-A.Brand";v="99", "Chromium";v="124"',
-            "Sec-Ch-Ua-Mobile": "?0",
-            "Sec-Ch-Ua-Platform": '"macOS"',
-            "Upgrade-Insecure-Requests": "1",
-            "Origin": "https://areariservata.federgolf.it",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6367.118 Safari/537.36",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "Sec-Fetch-Site": "same-origin",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-User": "?1",
-            "Sec-Fetch-Dest": "document",
-            "Referer": "https://areariservata.federgolf.it/CourseHandicapCalc/Index",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-            "Priority": "u=0, i",
-            "Connection": "close",
-        }
-
-        data = {
-            "selectedCircolo": selected_circolo_value,
-            "SelectedPercorso": selected_course_value,
-            "tee": tee_color,
-            "hcp": handicap,
-            "__RequestVerificationToken": st.session_state.antiforgery_token,
-        }
-
-        response = requests.post(url, headers=headers, data=data)
-
-        # print("Second Request Response Status Code:", response.status_code)
-        # print("Second Request Response Content:", response.content.decode())
-
-        # if response.status_code != 200:
-        #     return None
-
-        # Get course handicap
-        course_handicap = None
-
-        # Parse the HTML content
+    if selected_circolo_value:
+        # Get the soup and select the Percorso
         soup = BeautifulSoup(response.text, "html.parser")
+        course_options = {}
+        course_select = soup.select("#ddlPercorso option")
+        for option in course_select:
+            name = option.text.strip()
+            value = option["value"]
+            course_options[name] = value
 
-        # Find the table by its id
-        table = soup.find("table", id="risultatiHCP")
+        # Dropdown for selecting the course
+        selected_course = st.selectbox(
+            "Select Course", options=list(course_options.keys())
+        )
 
-        # Get all rows in the tbody
-        rows = table.find_all("tr")
+        # Getting the value based on the selected course name
+        selected_course_value = course_options[selected_course]
 
-        # Loop through the rows to find the Course Handicap
-        for row in rows:
-            columns = row.find_all("td")
-            if columns:
-                course_handicap = columns[4].get_text(
-                    strip=True
-                )  # 5th column is the Course Handicap
+        # Dropdown for Giallo and Rosso
+        tee_color = st.selectbox(
+            "Select Tee Color",
+            options=["Bianco", "Giallo", "Verde", "Blu", "Rosso", "Arancio"],
+        )
 
-        st.markdown(f"Course Handicap: {course_handicap}")
+        # Text field for handicap
+        handicap = st.text_input("Enter Handicap")
+
+        # Button to trigger the request
+        if st.button("Compute Handicap"):
+            url = "https://areariservata.federgolf.it/CourseHandicapCalc/Calc"
+
+            headers = {
+                "Host": "areariservata.federgolf.it",
+                "Cookie": f"ASP.NET_SessionId={st.session_state.session_id}; __RequestVerificationToken={st.session_state.request_verification_token}; ARRAffinity={st.session_state.arraffinity}; ARRAffinitySameSite={st.session_state.arraffinity_same_site}",
+                "Content-Length": "260",
+                "Cache-Control": "max-age=0",
+                "Sec-Ch-Ua": '"Not-A.Brand";v="99", "Chromium";v="124"',
+                "Sec-Ch-Ua-Mobile": "?0",
+                "Sec-Ch-Ua-Platform": '"macOS"',
+                "Upgrade-Insecure-Requests": "1",
+                "Origin": "https://areariservata.federgolf.it",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6367.118 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                "Sec-Fetch-Site": "same-origin",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-User": "?1",
+                "Sec-Fetch-Dest": "document",
+                "Referer": "https://areariservata.federgolf.it/CourseHandicapCalc/Index",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+                "Priority": "u=0, i",
+                "Connection": "close",
+            }
+
+            data = {
+                "selectedCircolo": selected_circolo_value,
+                "SelectedPercorso": selected_course_value,
+                "tee": tee_color,
+                "hcp": handicap,
+                "__RequestVerificationToken": st.session_state.antiforgery_token,
+            }
+
+            response = requests.post(url, headers=headers, data=data)
+
+            # print("Second Request Response Status Code:", response.status_code)
+            # print("Second Request Response Content:", response.content.decode())
+
+            # if response.status_code != 200:
+            #     return None
+
+            # Get course handicap
+            course_handicap = None
+
+            # Parse the HTML content
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            # Find the table by its id
+            table = soup.find("table", id="risultatiHCP")
+
+            # Get all rows in the tbody
+            rows = table.find_all("tr")
+
+            # Loop through the rows to find the Course Handicap
+            for row in rows:
+                columns = row.find_all("td")
+                if columns:
+                    course_handicap = columns[4].get_text(
+                        strip=True
+                    )  # 5th column is the Course Handicap
+
+            st.markdown(f"Course Handicap: {course_handicap}")
+
+            compute_handicap(course_handicap)
