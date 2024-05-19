7438536986fc436cdb2305058800dfd6894ae8b0
zacmiki <95082573+zacmiki@users.noreply.github.com>
2024-05-19 02:02:14 +0200

    Update hcp_functions.py
    Solved problem with wrong number of files before losing next valid round

--- modules/hcp_functions.py
+++ modules/hcp_functions.py
@@ -15,17 +15,18 @@ def loadcoursetable(dff):
     # Reset the index of strippeddf
     strippeddf = strippeddf.reset_index(drop=True)
 
-    # doing the same in one line
-    # best_8 = strippeddf.nlargest(8, 'Stbl')
-    best_8 = strippeddf.nlargest(8, "SD")
-    worst_8 = strippeddf.nsmallest(8, "SD")
-
-    min_SD_index = best_8["SD"].idxmin()
-    worstofbest = best_8.loc[min_SD_index]
-
-    element_number = strippeddf.index[strippeddf.eq(worstofbest).all(axis=1)][0]
-    nextgoodone = strippeddf.iloc[(min_SD_index - 1)]
-
+    #worst_8 = strippeddf.nsmallest(8, "SD")
+    best_8 = strippeddf.nsmallest(8, "SD")
+    
+    # Keep the original indices
+    best_8_indices = best_8.index
+    
+    # Find the maximum index value from the best_8 indices
+    max_index = best_8_indices.max()
+    
+    # Retrieve the corresponding row in the best_8 DataFrame
+    highest_indexed_element = best_8.loc[max_index]
+    
     strippeddf = strippeddf.rename(columns={"Index Nuovo": "New EGA"})
     strippeddf = strippeddf.rename(columns={"Date_String": "Date"})
     # --------- PAGE LAYOUT
@@ -42,15 +43,14 @@ def loadcoursetable(dff):
         icon="⛳️",
     )
     st.info(f"Your Next EXPIRING Round is")
-    st.subheader(f" {strippeddf.iloc[-1]['Gara']}")
-    st.subheader(
-        f"Date: {strippeddf.iloc[-1]['Date']} - Stableford = {strippeddf.iloc[-1]['Stbl']} - SD = {strippeddf.iloc[-1]['SD']}"
+    st.markdown(f"##### {strippeddf.iloc[-1]['Gara']}")
+    st.markdown(
+        f"##### Date: {strippeddf.iloc[-1]['Date']} - Stableford = {strippeddf.iloc[-1]['Stbl']} - SD = {strippeddf.iloc[-1]['SD']}"
     )
     st.divider()
-
     st.info(
-        f"""Apparently you have {20 - min_SD_index} games to play before you lose your next valid round which is  \n
-    	{worst_8.iloc[-1]['Gara']} - Stbl = {worst_8.iloc[-1]['Stbl']} - SD = {worst_8.iloc[-1]['SD']}"""
+        f"""Apparently you have {20 - max_index} games to play before you lose your next valid round which is  \n
+    	{highest_indexed_element['Gara']} - Stbl = {highest_indexed_element['Stbl']} - SD = {highest_indexed_element['SD']}"""
     )
 
     st.subheader("Last 20 VALID Rounds")
