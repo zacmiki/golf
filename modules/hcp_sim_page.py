b1468b52c437df2fd7f59dd33dca4322dae53486
zacmiki <95082573+zacmiki@users.noreply.github.com>
2024-05-20 12:28:05 +0200

    Update hcp_sim_page.py

--- modules/hcp_sim_page.py
+++ modules/hcp_sim_page.py
@@ -86,12 +86,12 @@ def new_hcp(sr_percorso, cr_percorso, par_percorso):
     new_sd = round(new_sd, 1)
 
     migliori_8 = np.append(migliori_8, new_sd)
-    st.write(migliori_8)
     best_8_SD = np.sort(migliori_8)[:8]
-    st.write(best_8_SD)
+    st.write(new_SD, best_8_SD)
     
     # Calculating the new HCP by taking the 96% of the avg(best 8)  
-    hcp_simulato = np.mean(best_8_SD) * 0.96
+    hcp_simulato = np.mean(best_8_SD)
+    #hcp_simulato = np.mean(best_8_SD) * 0.96
     hcp_simulato = round(hcp_simulato, 1)
 
     return new_sd, hcp_simulato
