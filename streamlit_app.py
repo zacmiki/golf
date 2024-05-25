25110eb4765bf06cf1a4fc9799d13dc8485b5c48
zacmiki <95082573+zacmiki@users.noreply.github.com>
2024-05-25 11:25:05 +0200

    SidebarButtonVersion
    changed the sidebar choice to buttons for better readability on mobile devices

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ from modules.hcp_manager_page import load_coursetable
 from modules.hcp_sim_page import hcp_sim
 from modules.login_federgolf import extract_data, login
 
-
 # Define a function to display the login form
 def display_login_form():
     st.title("Login to Load Your F.I.G. Results")
@@ -15,7 +14,6 @@ def display_login_form():
     submit_button = st.button("Login")
     return username, password, submit_button
 
-
 # Define a function to handle logout
 def handle_logout():
     st.sidebar.write("---")
@@ -23,21 +21,44 @@ def handle_logout():
     if logout_button:
         st.session_state.pop("logged_in", None)
         st.session_state.pop("df", None)
+        st.session_state.selected_option = "None"
         st.rerun()
 
+# Function to set the button style
+def sidebar_button(label):
+    button_html = f"""
+    <style>
+        div.stButton > button {{
+            width: 100%;
+        }}
+    </style>
+    """
+    st.sidebar.markdown(button_html, unsafe_allow_html=True)
+    return st.sidebar.button(label)
 
-# ---------------------   MAIN PAGE LOGIG --------------------
+# ---------------------   MAIN PAGE LOGIC --------------------
 # Main app logic
 def main():
-    selected_option = st.sidebar.selectbox(
-        "Select an option",
-        ["Official Rounds", "Handicap Manager", "Handicap Simulation"],
-    )
-
+    # Initialize the selected_option state if not set
+    if "selected_option" not in st.session_state:
+        st.session_state.selected_option = "None"
+    
     # Initialize the logged_in state if not set
     if "logged_in" not in st.session_state:
         st.session_state.logged_in = False
 
+    # Sidebar menu
+    st.sidebar.title("Your FederGolf Companion")
+    st.sidebar.caption("By Mic&Jac Zac")
+    st.sidebar.write("Please select an option from the sidebar.")
+    
+    if sidebar_button("Official Rounds"):
+        st.session_state.selected_option = "Official Rounds"
+    if sidebar_button("Handicap Manager"):
+        st.session_state.selected_option = "Handicap Manager"
+    if sidebar_button("Handicap Simulation"):
+        st.session_state.selected_option = "Handicap Simulation"
+    
     # Check if the user has already logged in
     if not st.session_state.logged_in:
         username, password, submit_button = display_login_form()
@@ -47,6 +68,7 @@ def main():
                 st.session_state.logged_in = login(username, password)
 
                 if st.session_state.logged_in:
+                    st.session_state.selected_option = "Handicap Manager"  # Set default to Handicap Manager
                     st.rerun()
                 else:
                     st.error(
@@ -55,7 +77,7 @@ def main():
             else:
                 st.error("Please enter both username and password.")
     else:
-        if selected_option == "Official Rounds":
+        if st.session_state.selected_option == "Official Rounds":
             # Make the request to extract the data
             if "df" not in st.session_state or st.session_state.df.empty:
                 st.session_state.df = extract_data()
@@ -66,18 +88,17 @@ def main():
             # Add a logout button in the sidebar
             handle_logout()
 
-        elif selected_option == "Handicap Simulation":
+        elif st.session_state.selected_option == "Handicap Simulation":
             if "df" not in st.session_state or st.session_state.df.empty:
                 st.session_state.df = extract_data()
 
             # User has already logged in, display the handicap visualizer
-            # hcp_sim(st.session_state.df)
             hcp_sim()
 
             # Add a logout button in the sidebar
             handle_logout()
 
-        elif selected_option == "Handicap Manager":
+        elif st.session_state.selected_option == "Handicap Manager":
             if "df" not in st.session_state or st.session_state.df.empty:
                 st.session_state.df = extract_data()
 
@@ -87,27 +108,20 @@ def main():
             # Add a logout button in the sidebar
             handle_logout()
         else:
-            pass
+            st.write("Please select an option from the sidebar.")
 
 
-# ------------- Visualization Page ------ F.I.G. Session -----------
-
-# Set up the sidebar
-st.sidebar.title("Your FederGolf Companion")
-st.sidebar.caption("By Mic&Jac Zac")
-st.sidebar.write("Please select an option from the sidebar.")
-
-
-if __name__ == "__main__":
-    main()
-
-# Add Buy Me a Coffee button to the sidebar
-st.sidebar.divider()
-st.sidebar.markdown(
+    st.sidebar.divider()
+    st.sidebar.markdown(
     """
     <a href="https://buymeacoffee.com/miczac?l=it" target="_blank">
         <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=YourUsername&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff">
     </a>
     """,
     unsafe_allow_html=True,
-)
+    )   
+
+# Set up the sidebar
+
+if __name__ == "__main__":
+    main()
