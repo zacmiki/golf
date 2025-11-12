7dd83770fc219b9103d4407934c912a543956ffb
zacmiki <95082573+zacmiki@users.noreply.github.com>
2025-11-12 13:29:05 +0100

    Update graphs.py

--- modules/graphs.py
+++ modules/graphs.py
@@ -103,3 +103,78 @@ def plot_last_n(n: int, plot_type="line", new_handicap=None):
     plt.tight_layout()
     st.pyplot(fig)
 
+
+# ------- Histogram with Gaussian Fit
+#st.cache_data
+def histo_n(plot_gaussian: bool = True, num_results: int = 100) -> None:
+    # Filter out non-finite values (None and zeros) from the DataFrame
+    
+    filtered_data = st.session_state.df["AGS"].dropna().replace(0, np.nan).dropna()
+
+    # Create a figure with a custom size
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    # Get the last num_results values from filtered_data
+    last_n_values = filtered_data.iloc[:num_results]
+
+    # Calculate the maximum and minimum values of last_n_values
+    max_value = last_n_values.max()
+    min_value = last_n_values.min()
+
+    # Create a histogram with fixed range bins and custom bin width
+    bins = range(70, int(max_value) + 4, 4)
+
+    hist, bins, _ = ax.hist(
+        last_n_values,
+        bins=bins,
+        edgecolor="black",
+        alpha=0.5,
+        color="lightblue",
+    )
+
+    # Fit a Gaussian distribution to the last_n_values
+    mu, std = norm.fit(last_n_values)
+
+    # Generate points along the Gaussian curve for smoother plotting
+    x_smooth = np.linspace(min_value - 10, max_value + 10, 1000)
+    gaussian_curve = (
+        norm.pdf(x_smooth, mu, std) * len(last_n_values) * np.diff(bins)[0]
+    )  # scaling by bin width
+
+    # Plot the Gaussian fit if the user wants to
+    if plot_gaussian:
+        ax.plot(x_smooth, gaussian_curve, "r--", linewidth=2)
+
+    # Add labels and title
+    ax.set_xlabel("Strokes per Round Distribution", fontsize=12)
+    ax.set_ylabel("Frequency", fontsize=12)
+    ax.set_title(
+        f"Strokes per round in the Last {num_results} FIG Tournaments", fontsize=12
+    )
+
+    # Show the grid
+    ax.grid(False)  # Remove grid lines
+
+    # Show both major and minor ticks
+    ax.minorticks_off()  # Turn off minor ticks
+
+    # Customize grid for major ticks
+    ax.grid(True, which="major", linestyle="-", linewidth=0.5)  # Adjust grid style
+
+    if plot_gaussian:
+        # Print the center value of the Gaussian
+        ax.text(
+            mu,
+            max(gaussian_curve) * 0.5,
+            f"Center: {mu:.2f}",
+            color="r",
+            ha="center",
+            fontsize='x-large',
+            fontweight = "demibold"
+        )
+        # Add a legend for the Gaussian fit
+        ax.legend(["Gaussian Fit"], loc="upper right", fontsize=8)
+
+    # Display the plot using Streamlit
+    st.pyplot(fig)
+
